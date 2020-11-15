d3e20ae619a241e64915e3523634b58b3d75f310
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-15 16:12:46 +0100

    Example test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 "use strict";
 const expect = require("chai").expect;
-const vueFetch = require("../dist/index.js");
+const vueFetch = require("../dist/index.js").default;
 
 describe("vueFetch function test", () => {
-  it("should return Success", () => {
-    let result = true;
-    expect(result).to.equal(true);
+  it("Should be loading", () => {
+    const response = vueFetch("https://jsonplaceholder.typicode.com/posts");
+    expect(response).to.be.an("Object");
+    expect(response.loading).to.equal(true);
+    expect(response.error).to.equal(null);
+    expect(response.data).to.equal(null);
   });
 });
