d0a36bfdcb8e02689f8e6ac1f2f4f39098e3e95e
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-15 15:57:54 +0100

    Installed mocha & chai. Example test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const expect = require("chai").expect;
+const vueFetch = require("../dist/index.js");
+
+describe("vueFetch function test", () => {
+  it("should return Success", () => {
+    let result = true;
+    expect(result).to.equal(true);
+  });
+});
